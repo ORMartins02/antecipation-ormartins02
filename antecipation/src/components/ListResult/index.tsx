cd1afe004cb0afef211c80e59c69f297cee092cb
Ricardo Martins <hikfm2@gmail.com>
2023-01-22 19:01:00 -0300

    fix: render ListResult

--- antecipation/src/components/ListResult/index.tsx
+++ antecipation/src/components/ListResult/index.tsx
@@ -2,25 +2,29 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 export const ListResult = () => {
-  const { antecipationValues } = useContext(AuthContext);
+  const { antecipationValues, actualResult } = useContext(AuthContext);
 
   return (
     <>
       <div className="days">
         <h3 className="key">Amanhã: </h3>
-        <h3 className="price"> R$ {antecipationValues["1"]},00</h3>
+        <h3 className="price"> R$ {antecipationValues["1"] | 0},00</h3>
       </div>
       <div className="days">
         <h3 className="key">Em 15 dias: </h3>
-        <h3 className="price">R$ {antecipationValues["15"]},00</h3>
+        <h3 className="price"> R$ {antecipationValues["15"] | 0},00</h3>
       </div>
       <div className="days">
         <h3 className="key">Em 30 dias: </h3>
-        <h3 className="price">R$ {antecipationValues["30"]},00</h3>
+        <h3 className="price"> R$ {antecipationValues["30"] | 0},00</h3>
+      </div>
+      <div className="days">
+        <h3 className="key">Em 60 dias: </h3>
+        <h3 className="price"> R$ {antecipationValues["60"] | 0},00</h3>
       </div>
       <div className="days">
         <h3 className="key">Em 90 dias: </h3>
-        <h3 className="price">R$ {antecipationValues["90"]},00</h3>
+        <h3 className="price"> R$ {antecipationValues["90"] | 0},00</h3>
       </div>
     </>
   );
--- antecipation/src/components/ReleaseForm/index.tsx
+++ antecipation/src/components/ReleaseForm/index.tsx
@@ -16,7 +16,7 @@ export const ReleaseForm = () => {
     <DivForm>
       <div>
         <h4>Simule sua Antecipação</h4>
-        <Form onChange={handleSubmit(onSubmitValue)}>
+        <Form onSubmit={handleSubmit(onSubmitValue)}>
           <div>
             <label htmlFor="amount">Informe o valor da venda *</label>
             <input
@@ -38,6 +38,19 @@ export const ReleaseForm = () => {
             <label htmlFor="mdr">Informe o percentual de MDR *</label>
             <input placeholder="" id="mdr" {...register("mdr")} />
           </div>
+          <div>
+            <label htmlFor="mdr">Selecione os períodos desejados</label>
+            <select id="days" {...register("days")}>
+              <option value=""></option>
+              <option value="01, 15, 30">01, 15, 30 dias</option>
+              <option value="15, 30">15, 30 dias</option>
+              <option value="15, 30, 60">15, 30, 60 dias</option>
+              <option value="30, 60">30, 60 dias</option>
+              <option value="30, 60, 90">30, 60, 90 dias</option>
+              <option value="60, 90">60, 90 dias</option>
+            </select>
+          </div>
+          <button>Simular</button>
         </Form>
       </div>
     </DivForm>
